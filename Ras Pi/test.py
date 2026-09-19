6e5f3bb3673e70a0de98fcc01fe2ab6377ac6fc7
L Forsyth <l.forsyth@ember.to>
2026-09-19 17:54:32 +0100

    last test

--- Ras Pi/test.py
+++ Ras Pi/test.py
@@ -3,15 +3,33 @@ import time
 
 pina = GPIO.OutputDevice(2)
 pinb = GPIO.OutputDevice(3)
+pinc = GPIO.OutputDevice(4)
+pind = GPIO.OutputDevice(5)
 def main():
-    pina.off()
-    pinb.off()
-    print("off for 15")
-    time.sleep(15)
     pina.on()
     pinb.on()
-    print("on for 15")
-    time.sleep(15)
+    pinc.on()
+    pind.on()
+    print("off for 5")
+    time.sleep(5)
+    pina.off()
+    pinb.off()
+    print("player A on for 1.5")
+    time.sleep(1.5)
+    pina.on()
+    pinb.on()
+    pinc.on()
+    pind.on()
+    pinc.off()
+    pind.off()
+    time.sleep(1.5)
+    print("player B on for 1.5")
+    pina.on()
+    pinb.on()
+    pinc.on()
+    pind.on()
+
+
 
 if __name__ == "__main__":
     while True:
