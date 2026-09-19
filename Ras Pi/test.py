3d836d4f956610b2a580ae2ac2ce204f27651281
L Forsyth <l.forsyth@ember.to>
2026-09-19 16:59:43 +0100

    new test

--- Ras Pi/test.py
+++ Ras Pi/test.py
@@ -6,9 +6,11 @@ pinb = GPIO.OutputDevice(3)
 def main():
     pina.off()
     pinb.off()
+    print("off for 15")
     time.sleep(15)
     pina.on()
     pinb.on()
+    print("on for 15")
     time.sleep(15)
 
 if __name__ == "__main__":
